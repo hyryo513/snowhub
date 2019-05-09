8668ed29a50b0500ce0c86d604e8849015551e28
Brian Cortez <brianolivares@me.com>
2019-05-08 19:10:39 -0500

    Create precache-manifest.ba9e112c11136dde3997a0927125af08.js

---
+++ client/build/precache-manifest.ba9e112c11136dde3997a0927125af08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc02e90b95e5349115dca3fc48fb095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263d82590a0b1ba146b9",
+    "url": "/static/css/main.7bb7c9ae.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "263d82590a0b1ba146b9",
+    "url": "/static/js/main.fd5b29f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
