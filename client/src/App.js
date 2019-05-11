4c79e87ddac9ded6c01f54c2389bd7d5831dc72b
Brian Cortez <brianolivares@me.com>
2019-05-11 10:48:27 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Login from "./Components/Containers/Login";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
-    return (
+  return (
+    <Router>
       <div>
-          <Navbar>
+        <Navbar>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route path="/login" exact component={Login} />
+            <Route component={NoMatch} />
+          </Switch>
         </Navbar>
-       
-        <Login></Login>
-      </div>  
-    )
+      </div>
+    </Router>
+  )
 }
 
 export default App;
