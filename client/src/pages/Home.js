af2910df692834813c0fb3a7363d4a57b9bd7e5c
Brian Cortez <brianolivares@me.com>
2019-05-11 10:55:54 -0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,17 +4,10 @@ import { Col, Row, Container } from "../Components/Grid";
 import Jumbotron from "../Components/Jumbotron";
 import API from "../utils/API";
 
-class Detail extends Component {
+class Home extends Component {
   state = {
     book: {}
   };
-  // When this component mounts, grab the book with the _id of this.props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-  componentDidMount() {
-    API.getBook(this.props.match.params.id)
-      .then(res => this.setState({ book: res.data }))
-      .catch(err => console.log(err));
-  }
 
   render() {
     return (
@@ -48,4 +41,4 @@ class Detail extends Component {
   }
 }
 
-export default Detail;
+export default Home;
